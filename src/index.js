ada84b064b00b2a40aaf1d3d6745cd9d4f32d6c2
Sam Pastoriza <pastorsj@rose-hulman.edu>
2017-02-18 11:58:08 -0500

    First commit to node library

---
+++ src/index.js
@@ -0,0 +1,12 @@
+'use strict';
+// import axios from 'axios';
+// import _ from 'lodash';
+
+class Fred {
+    constructor(apiKey) {
+        this.apiKey = apiKey;
+    }
+
+}
+
+export default Fred;
