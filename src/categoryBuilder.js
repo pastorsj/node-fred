c3b12ba81b1995180ae3645502ab45924dfdc3a7
Sam Pastoriza <pastorsj@rose-hulman.edu>
2017-02-20 16:45:30 -0500

    Added the rest of the category related api calls

--- src/categoryBuilder.js
+++ src/categoryBuilder.js
@@ -9,18 +9,118 @@ class CategoryBuilder extends Builder {
         return this.addAttribute('category_id=' + categoryId);
     }
 
-    setRealTimeStart(realTimeStart) {
-        if(realTimeStart === '') {
+    isValidDate(dateString) {
+        const regEx = /^\d{4}-\d{2}-\d{2}$/;
+
+        return dateString.match(regEx) != null;
+    }
+
+    setRealTimeStart(params) {
+        const realTimeStart = params['realtime_start'];
+
+        if(realTimeStart === '' || !this.isValidDate(realTimeStart)) {
             return this;
         }
         return this.addAttribute('realtime_start=' + realTimeStart);
     }
 
-    setRealTimeEnd(realTimeEnd) {
-        if(realTimeEnd === '') {
+    setRealTimeEnd(params) {
+        const realTimeEnd = params['realtime_end'];
+
+        if(realTimeEnd === '' || !this.isValidDate(realTimeEnd)) {
             return this;
         }
         return this.addAttribute('realtime_end=' + realTimeEnd);
+    }
+
+    setLimit(params) {
+        const limit = params['limit'];
+
+        if(limit === '') {
+            return this;
+        }
+        return this.addAttribute('limit=' + limit);
+    }
+
+    setOffset(params) {
+        const offset = params['offset'];
+
+        if(offset === '') {
+            return this;
+        }
+        return this.addAttribute('offset=' + offset);
+    }
+
+    setOrderBy(params) {
+        const orderBy = params['order_by'];
+
+        if(orderBy === '') {
+            return this;
+        }
+        return this.addAttribute('order_by=' + orderBy);
+    }
+
+    setSortOrder(params) {
+        const sortOrder = params['sort_oder'];
+
+        if(sortOrder === '') {
+            return this;
+        }
+        return this.addAttribute('sort_oder=' + sortOrder);
+    }
+
+    setFilterVariable(params) {
+        const filterVariable = params['filter_variable'];
+
+        if(filterVariable === '') {
+            return this;
+        }
+        return this.addAttribute('filter_variable=' + filterVariable);
+    }
+
+    setFilterValue(params) {
+        const filterValue = params['filter_value'];
+
+        if(filterValue === '') {
+            return this;
+        }
+        return this.addAttribute('filter_value=' + filterValue);
+    }
+
+    setTagNames(params) {
+        const tagNames = params['tag_names'];
+
+        if(tagNames === '') {
+            return this;
+        }
+        return this.addAttribute('tag_names=' + tagNames);
+    }
+
+    setExcludeTagNames(params) {
+        const excludeTagNames = params['exclude_tag_names'];
+
+        if(excludeTagNames === '') {
+            return this;
+        }
+        return this.addAttribute('exclude_tag_names=' + excludeTagNames);
+    }
+
+    setSearchText(params) {
+        const searchText = params['search_text'];
+
+        if(searchText === '') {
+            return this;
+        }
+        return this.addAttribute('search_text=' + searchText);
+    }
+
+    setTagGroupId(params) {
+        const tagGroupId = params['tag_group_id'];
+
+        if(tagGroupId === '') {
+            return this;
+        }
+        return this.addAttribute('tag_group_id=' + tagGroupId);
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -25,32 +25,90 @@ class Fred {
         return api.get('category?' + url);
     }
 
-    getCategoryChildren(categoryId, realTimeStart = '', realTimeEnd = '') {
+    getCategoryChildren(categoryId, params) {
         const url = this.categoryBuilder
             .setAPIKey(this.apiKey)
             .setCategoryId(categoryId)
             .setFileType(this.returnType)
-            .setRealTimeStart(realTimeStart)
-            .setRealTimeEnd(realTimeEnd)
+            .setRealTimeStart(params)
+            .setRealTimeEnd(params)
             .getUrl();
 
         return api.get('category/children?' + url);
     }
 
-    getReleases() {
-
-    }
-
-    getCategoryRelated(categoryId, realTimeStart = '', realTimeEnd = '') {
+    getCategoryRelated(categoryId, params) {
         const url = this.categoryBuilder
             .setAPIKey(this.apiKey)
             .setCategoryId(categoryId)
             .setFileType(this.returnType)
-            .setRealTimeStart(realTimeStart)
-            .setRealTimeEnd(realTimeEnd)
+            .setRealTimeStart(params)
+            .setRealTimeEnd(params)
             .getUrl();
 
         return api.get('category/related?' + url);
+    }
+
+    getCategorySeries(categoryId, params) {
+        const url = this.categoryBuilder
+            .setAPIKey(this.apiKey)
+            .setCategoryId(categoryId)
+            .setFileType(this.returnType)
+            .setRealTimeStart(params)
+            .setRealTimeEnd(params)
+            .setLimit(params)
+            .setOffset(params)
+            .setOrderBy(params)
+            .setSortOrder(params)
+            .setFilterVariable(params)
+            .setFilterValue(params)
+            .setTagNames(params)
+            .setExcludeTagNames(params)
+            .getUrl();
+
+        return api.get('category/series?' + url);
+    }
+
+    getCategoryTags(categoryId, params) {
+        const url = this.categoryBuilder
+            .setAPIKey(this.apiKey)
+            .setCategoryId(categoryId)
+            .setFileType(this.returnType)
+            .setRealTimeStart(params)
+            .setRealTimeEnd(params)
+            .setLimit(params)
+            .setOffset(params)
+            .setOrderBy(params)
+            .setSortOrder(params)
+            .setTagNames(params)
+            .setTagGroupId(params)
+            .setSearchText(params)
+            .getUrl();
+
+        return api.get('category/tags?' + url);
+    }
+
+    getCategoryRelatedTags(categoryId, params) {
+        const url = this.categoryBuilder
+            .setAPIKey(this.apiKey)
+            .setCategoryId(categoryId)
+            .setFileType(this.returnType)
+            .setRealTimeStart(params)
+            .setRealTimeEnd(params)
+            .setLimit(params)
+            .setOffset(params)
+            .setOrderBy(params)
+            .setSortOrder(params)
+            .setTagNames(params)
+            .setTagGroupId(params)
+            .setSearchText(params)
+            .getUrl();
+
+        return api.get('category/tags?' + url);
+    }
+
+    getReleases() {
+
     }
 
     getSeries() {
