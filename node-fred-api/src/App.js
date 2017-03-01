c79b3c103ab3ad15d35a155ba1759041e35238c4
Sam Pastoriza <pastorsj@rose-hulman.edu>
2017-03-01 14:26:18 -0500

    Updated to react version of semantic

--- node-fred-api/src/App.js
+++ node-fred-api/src/App.js
@@ -2,56 +2,95 @@ import React, { Component } from 'react';
 import './App.css';
 import api from './api.json';
 
+import { Header, Grid, Table, Icon, Segment, List, Container } from 'semantic-ui-react';
+
 class App extends Component {
 
     constructor(props) {
         super(props);
-        console.log('API', api);
+        this.state = {
+            api: api,
+            searchApi: api
+        };
     }
 
     render() {
         return (
-          <div className="ui segments">
-              {
-                  api && api.routes && Object.keys(api.routes).map((key) => {
-                      const route = api.routes[key];
-
-                      return (
-                          <div className="ui segment">
-                            <h1>{key}</h1>
-                            <h3>{route.method}</h3>
-                            <h4>{route.description}</h4>
-                            <span>Example: <code className="ui floating message"> <i className="terminal icon"></i> {route.example} </code></span>
-                            <h3> Parameters </h3>
-                                <table className="ui celled striped table">
-                                    <thead>
-                                        <tr>
-                                            <th> Route </th>
-                                            <th> Description </th>
-                                            <th> Type </th>
-                                            <th> Default Value </th>
-                                            <th> Required? </th>
-                                        </tr>
-                                    </thead>
+            <Container>
+                <Header as='h2' textAlign='center' icon>
+                    <Icon name='database' circular/>
+                    {api.title}
+                </Header>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={4}>
+                            <Segment raised fluid>
+                                <List>
                                     {
-                                        Object.keys(route.parameters).map((parameter) => {
+                                        api && api.routes && Object.keys(api.routes).map((key, index) => {
                                             return (
-                                                <tr>
-                                                    <th> {parameter} </th>
-                                                    <th> {route.parameters[parameter].description} </th>
-                                                    <th> {route.parameters[parameter].type} </th>
-                                                    <th> {route.parameters[parameter].default} </th>
-                                                    <th> {route.parameters[parameter].required} </th>
-                                                </tr>
+                                                <List.Item href={'#' + key} key={index}>{key}</List.Item>
                                             );
                                         })
                                     }
-                                </table>
-                            </div>
-                      );
-                  })
-              }
-          </div>
+                                </List>
+                            </Segment>
+                        </Grid.Column>
+                        <Grid.Column width={12}>
+                            {
+                                api && api.routes && Object.keys(api.routes).map((key, index) => {
+                                    const route = api.routes[key];
+
+                                    return (
+                                        <Segment id={key} key={index} raised>
+                                            <Header as='h1'>{key}</Header>
+                                            <Header as='h3'>{route.method}</Header>
+                                            <Header as='h4'>{route.description}</Header>
+                                            <Header as='h3'>Parameters</Header>
+                                            <Table celled padded stackable collapsing>
+                                                <Table.Header>
+                                                    <Table.Row>
+                                                        <Table.HeaderCell>Route</Table.HeaderCell>
+                                                        <Table.HeaderCell>Description</Table.HeaderCell>
+                                                        <Table.HeaderCell>Type</Table.HeaderCell>
+                                                        <Table.HeaderCell>Default Value</Table.HeaderCell>
+                                                        <Table.HeaderCell>Required?</Table.HeaderCell>
+                                                    </Table.Row>
+                                                </Table.Header>
+                                                <Table.Body>
+                                                    {
+                                                        Object.keys(route.parameters).map((parameter, index2) => {
+                                                            return (
+                                                                <Table.Row key={index2}>
+                                                                    <Table.Cell color='green'>
+                                                                        {parameter}
+                                                                    </Table.Cell>
+                                                                    <Table.Cell>
+                                                                        {route.parameters[parameter].description}
+                                                                    </Table.Cell>
+                                                                    <Table.Cell>
+                                                                        {route.parameters[parameter].type}
+                                                                    </Table.Cell>
+                                                                    <Table.Cell>
+                                                                        {route.parameters[parameter].default}
+                                                                    </Table.Cell>
+                                                                    <Table.Cell textAlign='center' verticalAlign='middle'>
+                                                                        {route.parameters[parameter].required ? (<Icon name='check' circular size='small' />) : (<Icon name='remove' circular size='small' />)}
+                                                                    </Table.Cell>
+                                                                </Table.Row>
+                                                            );
+                                                        })
+                                                    }
+                                                </Table.Body>
+                                            </Table>
+                                        </Segment>
+                                    );
+                                })
+                            }
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Container>
         );
     }
 }
